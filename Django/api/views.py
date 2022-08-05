6a6764ecc2006d3820741d801c64fc7ec14e5d0b
enginooby <enginooby@gmail.com>
2022-08-05 19:06:47 +0200

    Add Todo model and data in database

--- Django/api/views.py
+++ Django/api/views.py
@@ -1,9 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from todolist.models import Todo
+from .serializers import TodoSerializer
 
 
 @api_view(['GET'])
 def getTodoList(request):
-    todoList = [{'title': 'Todo1', 'status': 'In-progress'},
-                {'title': 'Todo2', 'status': 'Completed'}]
-    return Response(todoList)
+    # todoList = [{'title': 'Todo1', 'status': 'In-progress'},
+    #             {'title': 'Todo2', 'status': 'Completed'}]
+    
+    todolist = Todo.objects.all()
+    serializer = TodoSerializer(todolist, many=True)
+    return Response(serializer.data)
--- Django/todolist/models.py
+++ Django/todolist/models.py
@@ -1,3 +1,16 @@
+from enum import Enum
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Todo(models.Model):
+    class Status(models.TextChoices):
+        NotStarted = 'NS', _('Not started')
+        InProgress = 'IP', _('In-progress')
+        Completed = 'CO', _('Completed')
+
+    title = models.CharField(max_length=500)
+    content = models.CharField(max_length=1000)
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.InProgress)
+    time_created = models.DateTimeField(auto_now_add=True)
+    is_archived = models.BooleanField(default=False)
