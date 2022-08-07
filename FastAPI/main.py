ecea0e7ab582675e420878152dc942b9ed130e8d
enginooby <enginooby@gmail.com>
2022-08-07 20:17:34 +0200

    [FastAPI] Get all endpoint

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/api")
+async def getAll():
+    data = json.load(open('_Shared/todolist.json'))
+    return data
