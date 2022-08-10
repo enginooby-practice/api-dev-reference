538e62c9c917fe690b2606a8fcd32e29a3310809
enginooby <enginooby@gmail.com>
2022-08-10 11:10:19 +0200

    [ExpressJS] Implement search query for get-all route

--- ExpressJS/routes.js
+++ ExpressJS/routes.js
@@ -4,27 +4,35 @@ const router = express.Router();
 
 const path = require("path");
 const dataFilePath = path.join(__dirname, "./_Shared/todolist.json");
+const dataJson = fs.readFileSync(dataFilePath);
+let data = [];
+data = JSON.parse(dataJson);
 
 const getAll = async (req, res, next) => {
-  try {
-    const dataJson = fs.readFileSync(dataFilePath);
-    const data = JSON.parse(dataJson);
-    res.json(data);
-    // const countryStat = data.find(stat => stat.country.toUpperCase() == req.params.country.toUpperCase());
-    //
-    // if (!countryStat) {
-    //   const error = new Error(`Data for ${req.params.country} not found`);
-    //   error.status = 404;
-    //   throw error;
-    // }
-    // res.json(countryStat);
-  } catch (e) {
-    next(e);
-  }
+    try {
+        // search query
+        const titleQuery = req.query.title;
+        if (titleQuery) {
+            const filteredData = data.filter(e => e.title.toUpperCase().includes(titleQuery.toUpperCase()));
+            return res.json(filteredData);
+        }
+
+        return res.json(data);
+        // const countryStat = data.find(stat => stat.country.toUpperCase() == req.params.country.toUpperCase());
+        //
+        // if (!countryStat) {
+        //   const error = new Error(`Data for ${req.params.country} not found`);
+        //   error.status = 404;
+        //   throw error;
+        // }
+        // res.json(countryStat);
+    } catch (e) {
+        next(e);
+    }
 };
 
 router
-  .route("/api")
+    .route("/api")
     .get(getAll);
 
 module.exports = router;
