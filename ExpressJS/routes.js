045d709da9574ce23578e531aa2e1801dcbfe057
enginooby <enginooby@gmail.com>
2022-08-10 10:49:31 +0200

    [ExpressJS] Get-all route

--- ExpressJS/routes.js
+++ ExpressJS/routes.js
@@ -1,29 +1,30 @@
 const express = require("express");
 const fs = require("fs");
-const path = require("path");
-
 const router = express.Router();
-const dataFilePath = path.join(__dirname, "./corona-data.json");
 
-const getData = async (req, res, next) => {
+const path = require("path");
+const dataFilePath = path.join(__dirname, "./_Shared/todolist.json");
+
+const getAll = async (req, res, next) => {
   try {
-    const data = fs.readFileSync(dataFilePath);
-    const stats = JSON.parse(data);
-    const countryStat = stats.find(stat => stat.country.toUpperCase() == req.params.country.toUpperCase());
-
-    if (!countryStat) {
-      const error = new Error(`Data for ${req.params.country} not found`);
-      error.status = 404;
-      throw error;
-    }
-    res.json(countryStat);
+    const dataJson = fs.readFileSync(dataFilePath);
+    const data = JSON.parse(dataJson);
+    res.json(data);
+    // const countryStat = data.find(stat => stat.country.toUpperCase() == req.params.country.toUpperCase());
+    //
+    // if (!countryStat) {
+    //   const error = new Error(`Data for ${req.params.country} not found`);
+    //   error.status = 404;
+    //   throw error;
+    // }
+    // res.json(countryStat);
   } catch (e) {
     next(e);
   }
 };
 
 router
-  .route("/corona/:country")
-  .get(getData);
+  .route("/api")
+    .get(getAll);
 
 module.exports = router;
