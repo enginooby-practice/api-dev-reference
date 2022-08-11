d53547e7264c1b6a9f3fcf27ce6d4c61398f899f
enginooby <enginooby@gmail.com>
2022-08-11 21:21:07 +0200

    [ExpressJS] Add plain class for Task model

--- ExpressJS/src/entities/Task.ts
+++ ExpressJS/src/entities/Task.ts
@@ -1,30 +1,40 @@
 import {Schema, model, connect} from "mongoose"
 
-enum TodoStatus {
+export enum TaskStatus {
     NotStarted = "Not started",
     InProgress = "In-process",
     Completed = "Completed"
 }
 
-interface ITodo {
+interface ITask {
     id: number;
     title: string,
-    status: TodoStatus,
+    status: TaskStatus,
     timeCreated: Date,
     priority: number,
     isArchived: boolean,
     tags: string[],
 }
 
-const todoSchema = new Schema<ITodo>({
+export class Task {
+    id: number;
+    isArchived: boolean;
+    priority: number;
+    status: TaskStatus;
+    tags: string[];
+    timeCreated: Date;
+    title: string;
+}
+
+const taskSchema = new Schema<ITask>({
     id: {type: Number, required: false},
     title: {type: String, required: true},
     status: {type: String, required: false}, // ? Create custom Mongoose type from TodoStatus enum
     timeCreated: {type: Date, required: false},
-    priority: {type: Number, required: false},
-    isArchived: {type: Boolean, required: false},
+    priority: {type: Number, required: false, default: 3},
+    isArchived: {type: Boolean, required: false, default: false},
     tags: {type: [String], required: false}
 })
 
-export const Todo = model<ITodo>('Todo', todoSchema)
+export const TaskModel = model<ITask>('Task', taskSchema)
 
