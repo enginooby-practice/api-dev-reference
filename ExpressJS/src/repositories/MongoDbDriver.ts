6043b12898fa80021ef4378c254a0b8283a7a96c
enginooby <enginooby@gmail.com>
2022-08-12 08:41:05 +0200

    [ExpressJS] Create MongoDB Driver w/ global database operations

---
+++ ExpressJS/src/repositories/MongoDbDriver.ts
@@ -0,0 +1,30 @@
+import {MongoClient, Db, ObjectId} from "mongodb";
+import {connect} from "mongoose";
+import * as Mongoose from "mongoose";
+
+const connectionUrl = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+export class MongoDbDriver {
+    private static instance: MongoDbDriver;
+
+    private constructor() {
+    }
+
+    public static getInstance() {
+        if (!MongoDbDriver.instance) {
+            MongoDbDriver.connect();
+            MongoDbDriver.instance ??= new MongoDbDriver();
+        }
+
+        return MongoDbDriver.instance;
+    }
+
+    public static connect(): Promise<any> {
+        if (!MongoDbDriver.instance) {
+            return connect(`${connectionUrl}/${databaseName}`)
+                .then(r => console.log(">>> Connected to MongoDB database."))
+                .catch(e => console.log(e))
+        }
+    }
+}
