499ac6eb35f1e6ef8f75c8d06fa34e1b1a3689f0
enginooby <enginooby@gmail.com>
2022-08-16 08:09:20 +0200

    [ExpressJS] Refactor: merge router & controller layers
    Separating them results in many lazy functions and seems overengineered.

--- ExpressJS/src/routers/taskRouter.ts
+++ ExpressJS/src/routers/taskRouter.ts
@@ -1,16 +1,100 @@
-import Express from "express";
-import {createOne, deleteOne, getAll, getOne, updateOne} from "../controllers/taskController";
+import Express, {NextFunction, Request, Response} from "express";
 import {authHandler} from "../middlewares/authHandler";
+import {StatusCodes} from "http-status-codes";
+import {taskService} from "../services/TaskService";
+import {ITaskSorter, SortOrder, Task, TaskStatus} from "../entities/Task";
+import {IPaginator} from "../entities/IPaginator";
 
 export const taskRouter = Express.Router();
 
-taskRouter
-    .route("/")
-    .get(authHandler, getAll)
-    .post(authHandler, createOne)
+// get all
+taskRouter.get("/", authHandler, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const filter = parseFilter(req);
+        const paginator = parsePaginator(req);
+        const sorter = parseSorter(req);
 
-taskRouter
-    .route("/:id")
-    .get(authHandler, getOne)
-    .delete(authHandler, deleteOne)
-    .patch(authHandler, updateOne)
+        return res.status(StatusCodes.OK).json(await taskService.getAll(req.currentUser.id, req.query.title as string, filter, paginator, sorter));
+    } catch (e) {
+        next(e);
+    }
+});
+
+// get one
+taskRouter.get("/:id", authHandler, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        return res.status(StatusCodes.OK).json(await taskService.findById(req.currentUser.id, req.params.id))
+    } catch (e) {
+        next(e);
+    }
+})
+
+// delete one
+taskRouter.delete("/:id", authHandler, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        return res.status(StatusCodes.OK).json(await taskService.delete(req.currentUser.id, req.params.id))
+    } catch (e) {
+        next(e);
+    }
+})
+
+// create one
+taskRouter.post("/", authHandler, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        return res.status(StatusCodes.CREATED).json(await taskService.create(req.currentUser.id, req.body));
+    } catch (e) {
+        next(e);
+    }
+})
+
+// update one
+taskRouter.patch("/:id", authHandler, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        return res.status(StatusCodes.OK).json(await taskService.update(req.currentUser.id, req.params.id, req.body));
+    } catch (e) {
+        next(e);
+    }
+})
+
+function parseFilter(req: Request): Partial<Task> {
+    const filter: Partial<Task> = {};
+
+    if (req.query.isArchived) filter.isArchived = req.query.isArchived === "true";
+    if (req.query.priority) filter.priority = parseInt(req.query.priority as string);
+    if (req.query.status) filter.status = TaskStatus[getEnumKeyByValue(TaskStatus, req.query.status as string)];
+
+    return filter;
+}
+
+function parsePaginator(req: Request): IPaginator {
+    const paginator: IPaginator = {};
+
+    if (req.query.limit) {
+        paginator.limit = parseInt(req.query.limit as string);
+        if (req.query.page) paginator.page = parseInt(req.query.page as string);
+    }
+
+    return paginator;
+}
+
+function parseSorter(req: Request): ITaskSorter {
+    const sorter: ITaskSorter = {};
+
+    if (req.query.sortBy) {
+        const criteria = (req.query.sortBy as string).split(' '); // delimiter "+"
+        criteria.forEach((cri) => {
+            const partSplit = cri.split(':');
+            const sortKey = partSplit[0];
+            const sortOrder = partSplit[1].toLowerCase();
+            sorter[sortKey] = sortOrder === "desc" ? SortOrder.Desc : SortOrder.Esc;
+        })
+    }
+
+    return sorter;
+}
+
+// REFACTOR
+function getEnumKeyByValue(myEnum, enumValue): any {
+    let keys = Object.keys(myEnum).filter(x => myEnum[x] == enumValue);
+    return keys.length > 0 ? keys[0] : null;
+}
--- ExpressJS/src/routers/userRouter.ts
+++ ExpressJS/src/routers/userRouter.ts
@@ -1,28 +1,73 @@
-import Express from "express";
+// REFACTOR: duplicated try-catch
+import Express, {NextFunction, Request, Response} from "express";
 import {authHandler} from "../middlewares/authHandler";
-import {getProfile, signIn, signOut, signOutAll, signUp, deleteOne, updateOne} from "../controllers/userController";
+import {StatusCodes} from "http-status-codes";
+import {userService} from "../services/UserService";
 
 
 export const userRouter = Express.Router();
 
-userRouter
-    .route("/")
-    .post(signUp)
+// sign up
+userRouter.post("/", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        return res.status(StatusCodes.CREATED).json(await userService.signUp(req.body));
+    } catch (e) {
+        e.status = StatusCodes.BAD_REQUEST;
+        next(e);
+    }
+})
 
-userRouter
-    .route(`/login`)
-    .post(signIn)
+// sign in
+userRouter.post("/login", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        return res.status(StatusCodes.OK).send(await userService.signIn(req.body.email, req.body.password));
+    } catch (e) {
+        e.status = StatusCodes.BAD_REQUEST;
+        next(e);
+    }
+})
 
-userRouter
-    .route(`/logout`)
-    .post(authHandler, signOut)
+// sign out on 1 device
+userRouter.post("/logout", authHandler, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        return res.status(StatusCodes.OK).send(await userService.signOut(req.currentUser, req.currentToken));
+    } catch (e) {
+        next(e);
+    }
+})
 
-userRouter
-    .route(`/logoutAll`)
-    .post(authHandler, signOutAll)
+// sign out on all devices
+userRouter.post("/logoutAll", authHandler, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        return res.status(StatusCodes.OK).send(await userService.signOutAll(req.currentUser));
+    } catch (e) {
+        next(e);
+    }
+})
 
-userRouter
-    .route(`/me`)
-    .get(authHandler, getProfile)
-    .delete(authHandler, deleteOne)
-    .patch(authHandler, updateOne)
+// get profile
+userRouter.get("/me", authHandler, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        return res.status(StatusCodes.OK).json(req.currentUser);
+    } catch (e) {
+        next(e);
+    }
+})
+
+// update user
+userRouter.patch("/me", authHandler, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        return res.status(StatusCodes.OK).json(await userService.update(req.params.id, req.body));
+    } catch (e) {
+        next(e);
+    }
+})
+
+// delete user
+userRouter.delete("/me", authHandler, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        return res.status(StatusCodes.OK).json(await userService.delete(req.currentUser.id))
+    } catch (e) {
+        next(e);
+    }
+})
